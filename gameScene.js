ecd9320fb21bc40e07b84d89c1def1602ef2ebfd
Alam Husnan <kd.dunk81@gmail.com>
2022-12-13 11:38:16 +0500

    changes to spped of items

--- gameScene.js
+++ gameScene.js
@@ -25,8 +25,8 @@ let giftBox;
 let itemGroup;
 let sockOnlyGroup;
 let itemCount = 0;
-let speed = 2.5;
-let giftBoxSpeed = 3.5;
+let speed = 1.5;
+let giftBoxSpeed = 2;
 let oneTimeCount = 20;
 
 let score = 0;
